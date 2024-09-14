af6d163a30ab1012d52d696fd31c4ad0e7f382cf
Jordan Braz <jordanbraz0@gmail.com>
2024-09-14 18:54:58 -0300

    [PT-03] - add gorm

--- main.go
+++ main.go
@@ -1,129 +1,50 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/google/uuid"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type Product struct {
-	ID    string
+	ID    int `gorm:"primaryKey"`
 	Name  string
 	Price float64
 }
 
-func NewProduct(name string, price float64) *Product {
-	return &Product{
-		ID:    uuid.New().String(),
-		Name:  name,
-		Price: price,
-	}
-}
-
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
-	product := NewProduct("Shampoo", 12.0)
-	err = insertProduct(db, product)
-	if err != nil {
-		panic(err)
-	}
-	product.Price = 10.0
-	err = updateProduct(db, product)
-	if err != nil {
-		panic(err)
-	}
-	product, err = selectProduct(db, product.ID)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(product)
-	err = deleteProduct(db, product.ID)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Produto deletado")
-	products, err := selectAllProducts(db)
-	if err != nil {
-		panic(err)
-	}
-	for _, p := range products {
-		fmt.Printf("Product: %v, possui o preço de %.2f\n", p.Name, p.Price)
-	}
-}
 
-func insertProduct(db *sql.DB, product *Product) error {
-	stmt, err := db.Prepare("insert into products(id, name, price) values(?, ?, ?)")
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-	_, err = stmt.Exec(product.ID, product.Name, product.Price)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+	db.AutoMigrate(&Product{})
+	db.Create(&Product{
+		Name:  "D42",
+		Price: 100.0,
+	})
 
-func updateProduct(db *sql.DB, product *Product) error {
-	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
-	if err != nil {
-		return err
+	products := []Product{
+		{Name: "test1", Price: 1.0},
+		{Name: "test2", Price: 2.0},
+		{Name: "test3", Price: 3.0},
 	}
-	defer stmt.Close()
-	_, err = stmt.Exec(product.Name, product.Price, product.ID)
-	if err != nil {
-		return err
-	}
-	return nil
-}
 
-func selectProduct(db *sql.DB, id string) (*Product, error) {
-	stmt, err := db.Prepare("select * from products where id = ?")
-	if err != nil {
-		return nil, err
-	}
-	defer stmt.Close()
-	var p Product
-	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
-	if err != nil {
-		return nil, err
-	}
-	return &p, err
-}
+	db.Create(products)
 
-func deleteProduct(db *sql.DB, id string) error {
-	stmt, err := db.Prepare("DELETE FROM products WHERE id = ?")
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-	_, err = stmt.Exec(id)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+	// Read
+	var product Product
+	db.First(&product, 1)
+	db.First(&product, "name = ?", "test1")
 
-func selectAllProducts(db *sql.DB) ([]Product, error) {
-	rows, err := db.Query("select * from products")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-	var products []Product
-	for rows.Next() {
-		var p Product
-		err = rows.Scan(&p.ID, &p.Name, &p.Price)
-		if err != nil {
-			return nil, err
-		}
-		products = append(products, p)
-	}
-	return products, nil
+	// Update - update product's price to 200
+	db.Model(&product).Update("Price", 200)
+
+	// Update - update multiple fields
+	db.Model(&product).Updates(Product{Price: 200, Name: "F42"}) // non-zero fields
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Name": "F42"})
+
+	// Delete - delete product
+	db.Delete(&product, 1)
 }
